016f7c57a4074aee2df434229fa1f5d4e166f208
ssqwet1324 <sgerasimov407@gmail.com>
2025-08-17 11:07:02 +0300

    add L1.1

---
+++ Level L1/L1.1.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Human struct {
+	Name  string
+	Phone string
+}
+
+func (h *Human) SayHi() {
+	fmt.Printf("Hi, I am %s, my phone %s\n", h.Name, h.Phone)
+}
+
+func (h *Human) SayGoodbye() {
+	fmt.Printf("Goodbye %s, my phone %s\n", h.Name, h.Phone)
+}
+
+type Action struct {
+	Age string
+	Human
+}
+
+func main() {
+	//тут инициализируем структуру
+	a := Action{Age: "13", Human: Human{"Gik", "123"}}
+	//тут вызываем метод из Human благодаря встраиванию
+	a.SayHi()
+	a.SayGoodbye()
+	//тут выводим поля благодаря встраиванию
+	fmt.Println(a.Age, a.Name, a.Phone)
+}
