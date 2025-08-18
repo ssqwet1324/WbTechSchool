7d64a9dafb4ec6f3abfdffdde9fa0beb4426c279
ssqwet1324 <sgerasimov407@gmail.com>
2025-08-18 16:54:50 +0300

    change

--- Level L1/L1.2.go
+++ Level L1/L1.2.go
@@ -2,35 +2,19 @@ package main
 
 import "fmt"
 
-// берем числа из списка с помощью горутины
-func GiveNums(nums ...int) <-chan int {
-	c := make(chan int)
-
-	go func() {
-		for _, num := range nums {
-			c <- num
-		}
-		close(c)
-	}()
-	return c
-}
-
-// получаем числа с канала и возводим в квадрат
-func SqrtNums(num <-chan int) <-chan int {
-	c := make(chan int)
-
-	go func() {
-		for n := range num {
-			c <- n * n
-		}
-		close(c)
-	}()
-	return c
-}
-
-// вызываем
 func main() {
-	for n := range SqrtNums(GiveNums(2, 4, 6, 8, 10)) {
-		fmt.Println(n)
+	nums := []int{2, 4, 6, 8, 10}
+	done := make(chan bool)
+
+	for _, n := range nums {
+		go func(x int) {
+			fmt.Println(x * x)
+			done <- true
+		}(n)
+	}
+
+	// ждём, пока все горутины закончат
+	for range nums {
+		<-done
 	}
 }
