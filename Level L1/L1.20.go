a68750ca413d53ce1dd5521afe4d577624de9c98
ssqwet1324 <sgerasimov407@gmail.com>
2025-08-21 12:45:37 +0300

    add L1.20

--- Level L1/L1.20.go
+++ Level L1/L1.20.go
@@ -12,6 +12,29 @@ func ReverseWords(str string) {
 	}
 }
 
+func ReverseWordsRune(str string) {
+	runes := []rune(str)
+
+	//разворачиваем всю строку
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+
+	//разворачиваем каждое слово отдельно
+	start := 0
+	for i := 0; i <= len(runes); i++ {
+		if i == len(runes) || runes[i] == ' ' {
+			for l, r := start, i-1; l < r; l, r = l+1, r-1 {
+				runes[l], runes[r] = runes[r], runes[l]
+			}
+			start = i + 1
+		}
+	}
+
+	fmt.Println(string(runes))
+}
+
 func main() {
 	ReverseWords("snow dog sun")
+	ReverseWordsRune("snow dog sun")
 }
