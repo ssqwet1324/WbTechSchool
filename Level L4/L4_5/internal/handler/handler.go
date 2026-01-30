37c652c2302b1933263cb00bf5c1f0cf7de1c593
ssqwet1324 <sgerasimov407@gmail.com>
2026-01-30 19:29:30 +0300

    small fixes

--- Level L4/L4_5/internal/handler/handler.go
+++ Level L4/L4_5/internal/handler/handler.go
@@ -1,38 +1,3 @@
-//package handler
-//
-//import (
-//	"api_optimization/internal/entity"
-//	"api_optimization/internal/usecase"
-//	"net/http"
-//
-//	"github.com/gin-gonic/gin"
-//)
-//
-//// Handler - ручки
-//type Handler struct {
-//	uc *usecase.UseCase
-//}
-//
-//// New - конструктор
-//func New(uc *usecase.UseCase) *Handler {
-//	return &Handler{
-//		uc: uc,
-//	}
-//}
-//
-//// Sum - ручка обработки суммы
-//func (h *Handler) Sum(ctx *gin.Context) {
-//	var nums entity.Input
-//	if err := ctx.ShouldBindJSON(&nums); err != nil {
-//		ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-//		return
-//	}
-//
-//	sum := h.uc.ReturnSum(nums)
-//
-//	ctx.JSON(http.StatusOK, entity.Output{Sum: sum.Sum})
-//}
-
 package handler
 
 import (
